20f487c6df6cdeecdd098a7cd0433da45ba91bad
LookAtMeeZ <den.doc.00@gmail.com>
2016-06-02 13:39:10 +0300

    Observer + Errors

--- Zhabka/src/Start.java
+++ Zhabka/src/Start.java
@@ -1,25 +1,31 @@
-/**
- * Created by user on 6/2/2016.
- */
+import java.util.ArrayList;
+
 public class Start {
     public static void main(String[] args) {
 
         Value value = new Value();
         Printer currentDisplay = new Printer(value);
-        value.setMeasurements(13);
-        value.setMeasurements(55);
-        value.setMeasurements(55);
-        value.setMeasurements(44);
-        value.setMeasurements(44);
+        try{
+            value.setMeasurements(13);
+            value.setMeasurements(55);
+            value.setMeasurements(12);
+            value.setMeasurements(15);
+            value.setMeasurements(14);
+        } catch (TooManyRecords e){
+            System.out.println("Пойман");
+        }
+        finally{
+            System.out.println("Программа завершена");
+        }
     }
 }
 
 interface Observer {
-    void check(int temperature);
+    void check(int temperature) throws TooManyRecords;
 }
 
 interface Observable {
-    void notifyObservers();
+    void notifyObservers() throws TooManyRecords;
     void registerObserver(Observer o);
 }
 
@@ -32,17 +38,19 @@ class Value implements Observable {
         observer = o;
     }
     @Override
-    public void notifyObservers() {
+    public void notifyObservers() throws TooManyRecords {
         observer.check(temperature);
     }
 
-    public void setMeasurements(int temperature) {
+    public void setMeasurements(int temperature) throws TooManyRecords {
         this.temperature = temperature;
         notifyObservers();
     }
 }
 
 class Printer implements Observer {
+    int max = 4;
+    ArrayList<Integer> allValue = new ArrayList<>();
     private int temperature;
     private Value value;
 
@@ -51,9 +59,16 @@ class Printer implements Observer {
         value.registerObserver(this);
     }
     @Override
-    public void check(int temperature) {
+    public void check(int temperature) throws TooManyRecords {
         if (this.temperature != temperature) {
             this.temperature = temperature;
+            if (allValue.size() > max - 1 ) {
+                System.out.println(allValue);
+                throw new TooManyRecords(max);
+            }
+            allValue.add(temperature);
+            System.out.println(allValue);
+            System.out.println(allValue.size());
             display();
         }
     }
@@ -64,3 +79,12 @@ class Printer implements Observer {
 
 
 }
+class TooManyRecords extends Exception {
+    int max;
+
+    public TooManyRecords(int m) {
+        max = m;
+        System.out.println("Переполнение, максимальное кол-во элементов - " + max);
+    }
+}
+
