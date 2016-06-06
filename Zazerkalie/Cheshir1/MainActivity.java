97cdbce1b727944e8676fb88eff19ed918806387
Redish248 <irisha132132@mail.ru>
2016-06-06 14:17:41 +0300

    Add files via upload

---
+++ Zazerkalie/Cheshir1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.okno3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_main);
+
+
+    }
+}
