ab0b0a3cba4e67868507eae1d33b9265c3921fc9
LookAtMeeZ <den.doc.00@gmail.com>
2016-06-07 15:21:39 +0300

    DEMKA 11/10

--- Zhabka/2nd project/app/src/main/java/com/example/user/project/MainActivity.java
+++ Zhabka/2nd project/app/src/main/java/com/example/user/project/MainActivity.java
@@ -4,6 +4,8 @@
 
 package com.example.user.project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,52 +15,81 @@ import android.widget.ImageButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<prod> prodlist = new ArrayList<>();
-        final prod list = new  prod();
-        prodlist.add(list);
-        list.num= true;
-        final ImageButton b3 = (ImageButton)findViewById(R.id.imageButton);
-        final Switch switch1 = (Switch)findViewById(R.id.switch1);
-        final EditText editText = (EditText)findViewById(R.id.editText);
-        final Button b1 = (Button)findViewById(R.id.b1);
-        editText.setVisibility(View.INVISIBLE);
-        b1.setVisibility(View.INVISIBLE);
 
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Toast.makeText(MainActivity.this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show();
-                switch (v.getId())
-                {
-
-                    case R.id.imageButton:
-                        Toast.makeText(MainActivity.this, "Добавление", Toast.LENGTH_SHORT).show();
-                        editText.setVisibility(View.VISIBLE);
-                        b1.setVisibility(View.VISIBLE);
-                        case R.id.b1:
-                            list.str = editText.getText().toString();
-                            Toast.makeText(MainActivity.this, String.valueOf(list.str ), Toast.LENGTH_SHORT).show();
-
-                        break;
-                    case R.id.switch1:
-                        list.num = !list.num;
-                        Toast.makeText(MainActivity.this, String.valueOf(list.num ), Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
+        final ImageButton b3 = (ImageButton) findViewById(R.id.imageButton);
+        final ImageButton b2 = (ImageButton) findViewById(R.id.b2);
 
 
-        b3.setOnClickListener(listener);
-        switch1.setOnClickListener(listener);
-        b1.setOnClickListener(listener);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more informatixon.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.user.project/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.user.project/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+  public void onImage(View v) {
+      Intent intent = new Intent(this, Main2Activity.class);
+
+
+      startActivity(intent);
+
+  }
 }
