adfbe814fa14deccfd6d35b873f25a7df55533e9
ShockByte <darknetsoft@gmail.com>
2016-06-07 10:13:59 +0300

    Android update #2
    ololololo

--- River_Bandon/River_Bandon_Chat/src/Android/app/src/main/java/com/riverbandon/com/wifichat/SettingsActivity.java
+++ River_Bandon/River_Bandon_Chat/src/Android/app/src/main/java/com/riverbandon/com/wifichat/SettingsActivity.java
@@ -2,18 +2,12 @@ package com.riverbandon.com.wifichat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Client client;
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_settings);
     }
 }
