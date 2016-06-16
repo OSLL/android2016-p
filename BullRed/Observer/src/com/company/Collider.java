18755a488c29d308e2147973fb09b86b01e928d3
user <user>
2016-06-16 14:23:50 +0300

    Some extra classes

---
+++ BullRed/Observer/src/com/company/Collider.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by user on 6/16/2016.
+ */
+public class Collider
+{
+    public Vector2 point1;
+    public Vector2 point2;
+    public Collider (double x1, double y1, double x2, double y2)
+    {
+        point1 = new Vector2(x1, y1);
+        point2 = new Vector2(x2, y2);
+    }
+    public Collider (Vector2 vector1, Vector2 vector2)
+    {
+        point1 = vector1;
+        point2 = vector2;
+    }
+}
