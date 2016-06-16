55436c30d0585f9b86ce89300f3985a90d52b6c1
user <jojois@yandex.ru>
2016-06-16 15:10:26 +0300

    new project

---
+++ Programi4kof/superUplication228/app/src/main/java/ru/programi4koff/team/superuplication228/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.programi4koff.team.superuplication228;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Canvas;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends ActionBarActivity {
+    private DrawView testView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        testView = new DrawView(this);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics metricsB = new DisplayMetrics();
+        display.getMetrics(metricsB);
+
+
+
+        testView.cratePlayer(metricsB.widthPixels);
+       // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(testView);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        testView.update(event.getX(), event.getY());
+        return super.onTouchEvent(event);
+    }
+}
