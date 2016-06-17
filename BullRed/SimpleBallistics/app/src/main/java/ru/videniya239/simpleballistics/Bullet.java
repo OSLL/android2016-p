3ec189e953683431507a5309b2e04f9f94252bfe
user <user>
2016-06-17 13:35:34 +0300

    Added physics

--- BullRed/SimpleBallistics/app/src/main/java/ru/videniya239/simpleballistics/Bullet.java
+++ BullRed/SimpleBallistics/app/src/main/java/ru/videniya239/simpleballistics/Bullet.java
@@ -1,6 +1,7 @@
 package ru.videniya239.simpleballistics;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
 
 /**
  * Created by user on 6/17/2016.
@@ -8,6 +9,37 @@ import android.graphics.Canvas;
 public class Bullet
 {
     private Body body;
+    private Vector2 vector2;
+
+    float w; //WINDOWWIDTH
+    float h; //windowheight
+
+    float w_m;//условные размеры экрана в метрах
+    float h_m;
+
+    float aX;//ускорение
+    float aY;
+    float vX;//скорость снаряд
+    float vY;
+    float vwX;//скорость ветра
+    float vwY;
+    float posX;//текущая координата снаряда
+    float posY;
+    float posX0;//начальная координата снаряда
+    float posY0;
+
+    float alpha;
+    float modV;//модуль начальной скорости
+    float modVw;//модуль скорости ветра
+
+
+    float gX;//ускорение свободного падения
+    float gY;
+
+
+    int k;//коэффициенты их бы надо подоюрать
+    int m;
+
 
     public Body GetBody()
     {
@@ -16,98 +48,52 @@ public class Bullet
 
     public Bullet()
     {
-        Vector2 vector2 = new Vector2(10, 10);
+        w = GameController.screenWidth; //получаем разрешение экранчика
+        h = GameController.screenHeight;
+
+        w_m = 2000;//условные размеры экрана в метрах
+        h_m = w_m/w*h;
+        gX = 0;
+        gY = 10/w_m*w;
+
+        //v [300, 400] м/с
+        //вот эта параша должна вводиться
+        modV = 400; //Скорость
+        alpha = 50; //Угол
+        //
+
+        vX =  modV* (float)Math.cos(alpha*Math.PI/180)*w/w_m;
+        vY = -modV* (float)Math.sin(alpha*Math.PI/180)*w/w_m;
+
+        k = 200;//константы
+        m = 1500;
+
+        modVw = 10 ;//а вот эту парашу надо генерировать в каких-то границах
+        vwY = 0;//вводим ветерок
+        vwX = -modVw/w_m*w;
+
+
+        posX = 20;//начальное положение снаряда
+        posY = w-20;
+        posX0 = posX;
+        posY0 = posY;
+
     }
 
     public void Update(float deltaT)
     {
+        aX = (k / m) * (vX + (-vwX));//находим значение ускорения в данный момент времени
+        aY = -gY + (k / m) * vY;
 
+        vX = vX - (deltaT/1000) * aX;//находим значение скорости в данный момент времени
+        vY = vY - (deltaT/1000) * aY;
+
+        posX = posX + (deltaT/1000) * vX;//считая, что движение равномерное на deltaT находим новое положение снаряда
+        posY = posY + (deltaT/1000) * vY;
     }
 
     public void Draw(Canvas canvas)
     {
-
+        canvas.drawCircle(posX, posY, 5.0f, new Paint() );
     }
 }
-
-/*
- uses graphABC;
-2 type vector2 = record
-3   x : real;
-4   y : real;
-5
-6 end;
-7
-
-8 var a, g, v0, vv, v, pos, obl: vector2;
-9 dt, i, j, w, h : integer;
-10 k, m, w_m, h_m, alpha, modV, pi, pos0 : real;
-
-24 begin
-25   maximizewindow;
-26   w := WINDOWWIDTH;
-27   h := windowheight;
-28
-29   pi:=3.1415926535;
-30
-31   w_m:=2000; //óñëîâíûé ðàçìåð ýêðâíà â ìåòðàõ
-32   h_m:=w_m/w*h;
-33
-34   g.x := 0;
-35   g.y := 10/w_m*w;
-36
-37   //v [300, 400]ì/ñ
-38
-39   modV:=400;//â ìåòðàõ â ñåêóíäó
-40   alpha:=50;//â ãðàäóñàõ
-41
-42   v.x := modV*cos(alpha*pi/180)*w/w_m;
-43   v.y := -modV*sin(alpha*pi/180)*w/w_m;
-44
-45   k := 200;
-46   m := 1500;
-47
-48   vv.y := 0;
-49   vv.x := -10/w_m*w;//ñêîðîñòü âåòðà â ì/ñ
-50
-51   dt := 5;
-52
-53   pos.x := 20;
-54   pos.y := windowheight-20;
-55
-56   pos0:=pos.y;
-57
-58   setpencolor(clBlack);
-59   setbrushcolor(clBlack);
-60   fillcircle(trunc(pos.x), trunc(pos.y), 5);
-61   obl.x := w;
-62   obl.y := h div 10;
-63
-64   i := 0;
-65   while pos.y<=pos0 do
-66   begin
-67
-68     i:=i+1;
-69     if i mod 100 = 0 then fillcircle(trunc(pos.x), trunc(pos.y), 5);
-70
-71     a.x := (k / m) * (v.x + (-vv.x));
-72     a.y := -g.y + (k / m) * v.y;
-73
-74     v.x := v.x - (dt / 1000) * a.x;
-75     v.y := v.y - (dt / 1000) * a.y;
-76
-77     pos.x := pos.x + (dt / 1000) * v.x;
-78     pos.y := pos.y + (dt / 1000) * v.y;
-79
-80
-81     //sleep(dt div 5);
-82     //clearwindow;
-83   end; 
-93
-94   sleep(10000);
-95   halt;
-96
-97 end.
-
-
- */
