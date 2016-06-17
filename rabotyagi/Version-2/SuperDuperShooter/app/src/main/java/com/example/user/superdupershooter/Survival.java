8be242e952af4adea83fc4e9e84baf95ac63e5fb
user <user>
2016-06-17 15:06:16 +0300

    f On branch master

---
+++ rabotyagi/Version-2/SuperDuperShooter/app/src/main/java/com/example/user/superdupershooter/Survival.java
@@ -0,0 +1,19 @@
+package com.example.user.superdupershooter;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by user on 6/16/2016.
+ */
+public class Survival extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.player);
+        setContentView(new DrawView(this, this , pic));
+    }
+}
